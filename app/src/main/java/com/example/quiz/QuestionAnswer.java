e01723b9179120ca0c9e0bb239bc783c07bb475f
ParthGaud <131523355+ParthGaud@users.noreply.github.com>
2023-04-25 22:14:50 +0530

    first commit

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,48 @@
+package com.example.quiz;
+
+public class QuestionAnswer  {
+
+    public static String question[] ={
+            "Which number should be there next in this series? 25, 24, 22, 19, 15 ",
+            "Nia, twelve years old, is three times as old as her sister. How old will Nia be when she is twice as old as her sister? ",
+            "Which one of the five makes the best comparison? Brother is to sister as niece is to: ",
+            "Two people can make two bicycles in 2 hours. How many people are required to make 12 bicycles in 6 hours? ",
+            "Which of the following words is closest in meaning to APPREHENSIVE? ",
+            "Some months have 31 days; how many have 28? ",
+            "Which number is missing in : 144, ?, 206, 240 ",
+            "What is the name given to a group of HORSES?",
+            "BEGK : ADFJ :: PSVY : ",
+            "The day after the day after tomorrow is four days before Monday. What day is it today?"
+
+
+    };
+
+
+    public static String choices[][] ={
+            {"14","5","13","10"},
+            {"18","16","20","21"},
+            {"Mother","Aunt","Daughter","Nephew"},
+            {"6","4","2","1"},
+            {"Thorough","Foolish","Anxious","Comprehensive "},
+            {"1","3","6","12"},
+            {"155","167","170","174"},
+            {"husk","stud","mute","rush"},
+            {"ROUX","ORUX","LQUT","LOQT"},
+            {"Monday","Tuesday","Wednesday","Thursday"}
+
+    };
+
+    public static String correctAnswers[] ={
+            "10",
+            "16",
+            "Nephew",
+            "4",
+            "Anxious",
+            "12",
+            "174",
+            "stud",
+            "ORUX",
+            "Monday"
+    };
+
+}
